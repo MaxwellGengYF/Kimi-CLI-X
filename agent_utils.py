0d20e054d3bfd83e77c2d529c108b0c5acb5724e
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2026-03-16 14:14:36 +0800

    Update agent_utils.py

--- agent_utils.py
+++ agent_utils.py
@@ -144,7 +144,7 @@ def print_agent_json(get_message):
 
     def print_item(item):
         if type(item) == str:
-            if not (item.find('<choise>') >= 0 and item.find('</choise>') >= 0):
+            if not (item.find('<choice>') >= 0 and item.find('</choice>') >= 0):
                 print(item, end='\n')
         elif item.get("type") == "think":
             think_content = item.get("think", "")
@@ -154,7 +154,7 @@ def print_agent_json(get_message):
         elif item.get("type") == "text":
             text_content = item.get("text", "")
             if text_content:
-                if not (text_content.find('<choise>') >= 0 and text_content.find('</choise>') >= 0):
+                if not (text_content.find('<choice>') >= 0 and text_content.find('</choice>') >= 0):
                     print(f"\n{text_content}", end='\n')
         elif item.get("type") == "function":
             text = item.get("function", "")
@@ -168,7 +168,7 @@ def print_agent_json(get_message):
     if js.get("role") == "assistant":
         content = js.get("content", [])
         if type(content) == str:
-            if not (content.find('<choise>') >= 0 and content.find('</choise>') >= 0):
+            if not (content.find('<choice>') >= 0 and content.find('</choice>') >= 0):
                 print(content, end='\n')
             return
         for item in content:
